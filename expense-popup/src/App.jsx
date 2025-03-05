22e1bc6c4dad84034000dfb22e9f003636125594
Mugdha Shrivastava <shrivastavamugdha19@gmail.com>
2025-03-05 16:39:43 +0530

    Added expense-popup component

---
+++ expense-popup/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExpensePopup from "./ExpensePopup";
+
+function App() {
+  return (
+    <div>
+      <ExpensePopup />
+    </div>
+  );
+}
+
+export default App;
---
+++ expense-popup/src/ExpensePopup.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ExpensePopup.css";
+
+const ExpensePopup = () => {
+  return (
+    <div className="overlay">
+      <div className="popup">
+      <div className="popup-header">
+          <h3>Reset Expense Diary</h3>
+          <button className="close-btn">✖</button>
+        </div>
+
+        <p>Are you sure you want to reset your Expense diary? This will delete all logs and data prmanently, and this action cannot be undone </p>
+        <span className="small-text">Note: Resetting will erase all your budget settings, expense logs,and saved data.Proceed with caution</span>
+        <div className="buttons">
+          <button className="cancel">Cancel</button>
+          <button className="done">Done</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensePopup;
