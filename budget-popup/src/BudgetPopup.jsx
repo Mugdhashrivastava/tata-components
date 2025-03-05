c9cba2045f347ac233d6f8263f159581ea1028c5
Mugdha Shrivastava <shrivastavamugdha19@gmail.com>
2025-03-05 16:19:33 +0530

    cross btn

--- budget-popup/src/BudgetPopup.jsx
+++ budget-popup/src/BudgetPopup.jsx
@@ -5,7 +5,12 @@ const BudgetPopup = () => {
   return (
     <div className="overlay">
       <div className="popup">
-        <h3>Edit budget amount</h3>
+
+      <div className="popup-header">
+      <h3>Edit budget amount</h3>
+          <button className="close-btn">✖</button>
+        </div>
+        
         <input type="text"  />
         <div className="buttons">
           <button className="cancel">Cancel</button>
