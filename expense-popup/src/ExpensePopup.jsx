700e1557ac44e0c08abac12cc67c8191ff492402
Mugdha Shrivastava <shrivastavamugdha19@gmail.com>
2025-03-10 11:11:06 +0530

    x btn

--- expense-popup/src/ExpensePopup.jsx
+++ expense-popup/src/ExpensePopup.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./ExpensePopup.css";
 
 const ExpensePopup = () => {
-  return (
+  const [isOpen, setIsOpen] = useState(true);
+
+  return isOpen && (
     <div className="overlay">
       <div className="popup">
       <div className="popup-header">
           <h3 className="heading">Reset Expense Diary</h3>
-          <button className="close-btn">✖</button>
+          <button className="close-btn" onClick={() => setIsOpen(false)}>✖</button>
         </div>
 
         <p className="text">Are you sure you want to reset your Expense diary? This will delete all logs and data prmanently, and this action cannot be undone </p>
         <span className="small-text">Note: Resetting will erase all your budget settings, expense logs,and saved data.Proceed with caution</span>  
         <div className="buttons">
-          <button className="cancel">Cancel</button>
+        <button className="cancel" onClick={() => setIsOpen(false)}>Cancel</button>
           <button className="done">Done</button>
         </div>
       </div>
