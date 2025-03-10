a405e42b2cd6dec10dd6a7f4bfe19cf035a9ddf4
Mugdha Shrivastava <shrivastavamugdha19@gmail.com>
2025-03-10 11:04:14 +0530

    close btn

--- budget-popup/src/BudgetPopup.jsx
+++ budget-popup/src/BudgetPopup.jsx
@@ -5,20 +5,25 @@ import { useState } from "react";
 
 const BudgetPopup = () => {
   const [inputValue, setInputValue] = useState("₹ 40,00,000");
-  return (
+  const [isOpen, setIsOpen] = useState(true);
+
+  return isOpen && (
+
     
     <div className="overlay">
       <div className="popup">
 
       <div className="popup-header">
       <h3 className="heading">Edit budget amount</h3>
-          <button className="close-btn">✖</button>
+      <button className="close-btn" onClick={() => setIsOpen(false)}>✖</button>
+
         </div>
         
         <input type="text"  value={inputValue}
       onChange={(e) => setInputValue(e.target.value)} />
         <div className="buttons">
-          <button className="cancel">Cancel</button>
+        <button className="cancel" onClick={() => setIsOpen(false)}>Cancel</button>
+
           <button className="done">Done</button>
         </div>
       </div>
