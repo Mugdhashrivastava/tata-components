e25544c02527dce859623ec43f0887c9c8d8781d
Mugdha Shrivastava <shrivastavamugdha19@gmail.com>
2025-03-13 12:29:49 +0530

    d3

--- timeline-chart/src/App.jsx
+++ timeline-chart/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-// import Dashboard from "./Dashboard";
+
 import TimelineChart from "../../timeline-chart/src/TimelineChart";
 
 const App = () => {
   return (
     <div>
-      {/* <Dashboard /> */}
+   
       <TimelineChart />
     </div>
   );
