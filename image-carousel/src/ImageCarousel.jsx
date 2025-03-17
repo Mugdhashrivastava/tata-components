dacdfb4c8d05eb7ad277f5cb923f114dd05ac749
Mugdha Shrivastava <shrivastavamugdha19@gmail.com>
2025-03-17 12:26:36 +0530

    swiper

--- image-carousel/src/ImageCarousel.jsx
+++ image-carousel/src/ImageCarousel.jsx
@@ -1,27 +1,22 @@
 import { useState } from "react";
-import Slider from "react-slick";
 import { X } from "lucide-react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Thumbs } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
 import "./ImageCarousel.css";
 
 import img1 from "./assets/images/img.jpg";
 import img2 from "./assets/images/img2.jpg";
 import img3 from "./assets/images/img.jpg";
 
-const images = [img1, img2, img3];
+const images = [img1, img2, img3, img1, img2];
 
 export default function ImageCarousel({ isOpen, setIsOpen }) {
   if (!isOpen) return null;
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-  };
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   return (
     <div className="modal-overlay">
@@ -31,13 +26,35 @@ export default function ImageCarousel({ isOpen, setIsOpen }) {
         </button>
         <h2 className="text-lg font-semibold text-gray-800 mb-3">Reference photos/bill</h2>
 
-        <Slider {...settings} className="image-slider">
+        {/* Main Image Slider */}
+        <Swiper
+          loop = {true}
+          modules={[Navigation, Thumbs]}
+          navigation
+          thumbs={{ swiper: thumbsSwiper }}
+          spaceBetween={10}
+          className="main-swiper"
+        >
           {images.map((img, index) => (
-            <div key={index} className="slide">
+            <SwiperSlide key={index}>
               <img src={img} alt="Reference" className="carousel-image" />
-            </div>
+            </SwiperSlide>
           ))}
-        </Slider>
+        </Swiper>
+
+        {/* Thumbnail List */}
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          spaceBetween={10}
+          slidesPerView={5}
+          className="thumb-swiper"
+        >
+          {images.map((img, index) => (
+            <SwiperSlide key={index}>
+              <img src={img} alt="Thumbnail" className="thumbnail" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
