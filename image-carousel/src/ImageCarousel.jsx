3f5c5b982c312b8f37206ae72f00484bd10ba51a
Mugdha Shrivastava <shrivastavamugdha19@gmail.com>
2025-03-17 11:29:51 +0530

    no headless library

--- image-carousel/src/ImageCarousel.jsx
+++ image-carousel/src/ImageCarousel.jsx
@@ -1,21 +1,15 @@
 import { useState } from "react";
-import { Dialog } from "@headlessui/react";
 import { X } from "lucide-react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./ImageCarousel.css";
 
-
-
-
 import img1 from "./assets/images/img.jpg";
 import img2 from "./assets/images/img2.jpg";
 import img3 from "./assets/images/img.jpg";
 
-const images = [
-img1,img2,img3
-];
+const images = [img1, img2, img3];
 
 export default function ImageCarousel({ isOpen, setIsOpen }) {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -26,23 +20,23 @@ export default function ImageCarousel({ isOpen, setIsOpen }) {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    beforeChange: (oldIndex, newIndex) => setCurrentIndex(newIndex),
-    nextArrow: <SampleNextArrow />, 
+    beforeChange: (_, newIndex) => setCurrentIndex(newIndex),
+    nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
   };
 
   function SampleNextArrow(props) {
-    const { onClick } = props;
-    return <div className="slick-next" onClick={onClick}></div>;
+    return <div className="slick-next" onClick={props.onClick}></div>;
   }
 
   function SamplePrevArrow(props) {
-    const { onClick } = props;
-    return <div className="slick-prev" onClick={onClick}></div>;
+    return <div className="slick-prev" onClick={props.onClick}></div>;
   }
 
+  if (!isOpen) return null;
+
   return (
-    <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="modal-overlay">
+    <div className="modal-overlay">
       <div className="modal-content">
         <button onClick={() => setIsOpen(false)} className="close-btn">
           <X size={20} />
@@ -67,6 +61,6 @@ export default function ImageCarousel({ isOpen, setIsOpen }) {
           ))}
         </div>
       </div>
-    </Dialog>
+    </div>
   );
 }
