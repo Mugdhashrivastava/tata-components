a2f848076772a25d958b951613825926ce73da9a
Mugdha Shrivastava <shrivastavamugdha19@gmail.com>
2025-03-27 15:50:04 +0530

    finally clientlib is being created in ui frontend because of webpackcommon.js

--- separate-clientlib/webpackcommon.js
+++ separate-clientlib/webpackcommon.js
@@ -1,4 +1,4 @@
-use strict';
+'use strict';
 
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -7,101 +7,120 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
-const SOURCE_ROOT = path.join(__dirname, 'src/main/webpack');
+const SOURCE_ROOT = path.join(__dirname, '/src/main/webpack');
 
 const resolve = {
-    extensions: ['.js', '.ts'],
-    plugins: [
-        new TSConfigPathsPlugin({
-            configFile: './tsconfig.json'
-        })
-    ]
+  extensions: ['.js', '.jsx', '.ts', '.tsx', '.scss'],
+  plugins: [new TSConfigPathsPlugin({ configFile: './tsconfig.json' })]
 };
 
 module.exports = {
-    resolve: resolve,
-    entry: {
-        site: `${SOURCE_ROOT}/site/main.ts`,
-        dependencies: `${SOURCE_ROOT}/dependencies/main.ts`,
-      
-        custom: `${SOURCE_ROOT}/custom/main.ts` // New custom entry point
+  resolve,
+  entry: {
+    site: path.join(SOURCE_ROOT, '/site/main.ts'),
+    dependencies: path.join(SOURCE_ROOT, '/dependencies/main.ts'),
+    expenseDiary: path.join(SOURCE_ROOT, '/expense/main.ts') // Expense-specific entry
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: (chunkData) => {
+      const chunkName = chunkData.chunk.name;
+      if (chunkName === 'dependencies') {
+        return 'clientlib-dependencies/[name].js';
+      } else if (chunkName === 'expenseDiary') {
+        return 'clientlib-expense/[name].js';
+      } else if (chunkName === 'vendors') {
+        return 'clientlib-dependencies/[name].js'; // Vendors will be placed with expense if needed
+      }
+      return 'clientlib-site/[name].js';
     },
-    output: {
-        filename: (chunkData) => {
-            const chunkName = chunkData.chunk.name;
-            if (chunkName === 'dependencies') return 'clientlib-dependencies/[name].js';
-            if (chunkName === 'custom') return 'clientlib-custom/[name].js'; // Unified custom output
-            return 'clientlib-site/[name].js';
-        },
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    { loader: 'ts-loader' },
-                    {
-                        loader: 'glob-import-loader',
-                        options: { resolve: resolve }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: { url: false }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [require('autoprefixer')]
-                            }
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sassOptions: {
-                                includePaths: [path.resolve(__dirname, 'src/main/webpack')]
-                            }
-                        }
-                    },
-                    {
-                        loader: 'glob-import-loader',
-                        options: { resolve: resolve }
-                    }
-                ]
-            }
-        ] 
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new ESLintPlugin({ extensions: ['js', 'ts', 'tsx'] }),
-        new MiniCssExtractPlugin({
-            filename: (chunkData) => {
-                const chunkName = chunkData.chunk.name;
-                if (chunkName === 'dependencies') return 'clientlib-dependencies/[name].css';
-                if (chunkName === 'custom') return ' clientlib-custom/[name].css'; // Unified custom output
-                return 'clientlib-site/[name].css';
-            }
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: `${SOURCE_ROOT}/resources`, to: './clientlib-site/' }
-            ]
-        })
-    ],
-    stats: {
-        assetsSort: 'chunks',
-        builtAt: true,
-        errors: true,
-        errorDetails: true,
-        warnings: true
+    clean: true // Clean dist folder before every build
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'glob-import-loader',
+            options: { resolve: '../components/expense-diary' }
+          },
+          {
+            loader: 'ts-loader',
+            options: { configFile: 'tsconfig.json' }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { url: false } },
+          {
+            loader: 'postcss-loader',
+            options: { postcssOptions: { plugins: [require('autoprefixer')] } }
+          },
+          'sass-loader',
+          {
+            loader: 'glob-import-loader',
+            options: { resolve: '../components/expense-diary' }
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-react'] }
+        }]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: 'vendors', // Ensure generated vendor chunk is named "vendors"
     }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ESLintPlugin({ extensions: ['js', 'jsx', 'ts', 'tsx'] }),
+    new MiniCssExtractPlugin({
+      filename: (chunkData) => {
+        const chunkName = chunkData.chunk.name;
+        if (chunkName === 'dependencies') {
+          return 'clientlib-dependencies/[name].css';
+        } else if (chunkName === 'expenseDiary' || chunkName === 'vendors') {
+          return 'clientlib-expense/[name].css';
+        }
+        return 'clientlib-site/[name].css';
+      }
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(SOURCE_ROOT, 'resources'), to: './clientlib-site/' }
+      ]
+    })
+  ],
+  stats: {
+    assetsSort: 'chunks',
+    builtAt: true,
+    children: false,
+    chunkGroups: true,
+    chunkOrigins: true,
+    colors: false,
+    errors: true,
+    errorDetails: true,
+    env: true,
+    modules: false,
+    performance: true,
+    providedExports: false,
+    source: false,
+    warnings: true
+  }
 };
+
+
+
